793fa63374aea78bdcf88180a15634ed2504cd6a
DAIKI URATA <7nohe.urata@gmail.com>
2021-03-01 19:06:57 +0900

    Initial Commit

---
+++ PencilDrawing/PencilDrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PencilDrawingApp.swift
+//  PencilDrawing
+//
+//  Created by DAIKI URATA on 2021/03/01.
+//
+
+import SwiftUI
+
+@main
+struct PencilDrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
